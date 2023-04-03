4d227320b3f28e2c4b25e5d91b77931e6639762a
bertronika <nejc@bertoncelj.eu.org>
2023-04-03 20:40:25 +0200

    Dodaj statično stran, spravi osnovo v pogon

--- main.py
+++ main.py
@@ -1,68 +1,98 @@
 # Zaledje za Delilnico
 # NB (NUKS) 7. 3. 2023
 
-from typing import Union
-from fastapi import FastAPI, Response, Request, HTTPException
+# https://docs.sqlalchemy.org/en/20/orm/session_basics.html#querying
+# https://docs.sqlalchemy.org/en/20/tutorial/data_select.html#the-where-clause
+
+# FastAPI + zunanja razširitev za različice
+from fastapi import FastAPI, Response, Request, HTTPException, Form, Depends
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from fastapi_versioning import version, VersionedFastAPI
+from pydantic import BaseModel
+
+# Podatkovna baza
+# from typing import Union
 from db import engine, Base, Fragment
 from sqlalchemy.orm import Session
 from sqlalchemy import select
+Base.metadata.create_all(engine)
+
+# Javno izpostavljeni parametri APIja iz schemas.py
 import schemas
 
-Base.metadata.create_all(engine)
 app = FastAPI()
+templates = Jinja2Templates(directory="static")
 
 @app.get("/")
-def read_root():
-    """Privzeta vhodna točka"""
+@version(1)
+def home_page(request: Request):
+    """Domača stran Delilnice"""
+    return templates.TemplateResponse("index.html", {"request": request})
 
-    #return {"sporočilo": "Pozdrav."}
-    return "Domača stran Delilnice"
+class AddForm:
+    def __init__(self, title: str = Form(...), author: str = Form(...), text: str = Form(...), is_private: str = Form(False)):
+        self.title = title
+        self.author = author
+        self.text = text
+        self.is_private = is_private
 
+# @app.post("/add", status_code=201, response_class=HTMLResponse)
 @app.post("/add", status_code=201)
 @version(1)
-def add_fragment(frag: schemas.Fragment, request: Request, response: Response):
+# def add_fragment(frag: schemas.Fragment, request: Request, response: Response):
+# def add_fragment(add_form: AddForm = Depends()):
+def add_fragment(request: Request, response: Response, add_form: AddForm = Depends()):
     """Dodaj nov fragment"""
 
     client_host = request.client.host
+    print(add_form.is_private)
     #session = Session(bind=engine, expire_on_commit=False)
     with Session(engine) as session:
         fragment = Fragment(
             ip_addr=str(client_host),
             expiry_date="-",
-            title=frag.title,
-            author=frag.author,
-            text=frag.text,
-            is_private=False
+            title=add_form.title,
+            author=add_form.author,
+            text=add_form.text,
+            is_private=(add_form.is_private == "on")
         )
 
         session.add(fragment)
         session.commit()
-        print("Dodan fragment " + str(fragment.id))
+        print(f"USPEH - Vnašam fragment, oznaka {str(fragment.id)}")
 
-    return f"Vnašam fragment, oznaka {str(fragment.id)}"
+    return {"success": True, "id": fragment.id}
 
 @app.get("/fragment/{id}")
 @version(1)
-def retrieve_fragment(id: int, response: Response):
+def retrieve_fragment(id: int, request: Request, response: Response):
     """Pridobi fragment po podanem ID-ju"""
 
     frag_list = []
+    # frag = None
 
     statement = select(Fragment).where(Fragment.id==id)
     with Session(engine) as session:
         for frag in session.scalars(statement):
             frag_list.append(frag)
+            # frag = session.scalars(statement)
 
     if len(frag_list) < 1:
-        #response.status_code = 404
+    # if frag == None:
+        response.status_code = 404
         # TODO tudi za is_private
 
-        raise HTTPException(status_code=404, detail=f"Fragment {id} ne obstaja.")
+        # raise HTTPException(status_code=404, detail=f"Fragment {id} ne obstaja.")
 
-        return "Ta fragment ne obstaja"
+        print("NAPAKA - fragment ne obstaja")
+        retval = {"success": False, "reason": "Ta fragment ne obstaja."}
+    else:
+        print(f"USPEH - {frag_list}")
+        retval = {"success": True, "fragment": frag_list[0]}
 
-    return frag_list
+    return templates.TemplateResponse("retrieve_fragment.html", {"request": request, "retval": retval})
 
 @app.get("/author/{author}")
 @version(1)
@@ -80,9 +110,11 @@ def retrieve_fragments_from_author(author: str, response: Response):
 
     if len(frag_list) < 1:
         response.status_code = 404
-        return "Ta avtor nima lastnih fragmentov"
+        print("NAPAKA - avtor nima fragmentov")
+        return {"success": False, "reason": "Ta avtor nima lastnih fragmentov."}
 
-    return frag_list
+    print(f"USPEH - {frag_list}")
+    return {"success": True, "fragments": frag_list}
 
 # https://stackoverflow.com/questions/31624530/return-sqlalchemy-results-as-dicts-instead-of-lists
 @app.get("/all_fragments")
@@ -101,11 +133,12 @@ def retrieve_all_fragments(only_meta: bool=False):
             frag_list.append(frag._asdict())
 
     if len(frag_list) < 1:
-        return "Fragmentov še ni..."
+        print("NAPAKA - v zbirki ni nobenih fragmentov")
+        return {"success": False, "reason": "V zbirki še ni fragmentov."}
 
-    return frag_list
+    print(f"USPEH - {frag_list}")
+    return {"success": True, "fragments": frag_list}
 
-app = VersionedFastAPI(app, version_format="{major}", prefix_format="/v{major}")
 # @app.delete("/delete/{id}")
 #@version(1)
 # def delete_fragment(id: int):
@@ -113,4 +146,6 @@ app = VersionedFastAPI(app, version_format="{major}", prefix_format="/v{major}")
 #
 #     return success
 
-
+app = VersionedFastAPI(app, version_format="{major}", prefix_format="/v{major}")
+# app.mount("/", StaticFiles(directory="static", html=True), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
--- schemas.py
+++ schemas.py
@@ -1,10 +1,21 @@
 from pydantic import BaseModel
+from typing import Annotated
+from fastapi import Form
 
 class Fragment(BaseModel):
     """
     Podatki, javno izpostavljeni prek API-ja.
     """
-    title: str
-    author: str
-    text: str
-    is_private: bool
+    title: Annotated[str, Form()]
+    author: Annotated[str, Form()]
+    text: Annotated[str, Form()]
+    is_private: Annotated[bool, Form()]
+
+# class Fragment(BaseModel):
+#     """
+#     Podatki, javno izpostavljeni prek API-ja.
+#     """
+#     title: str
+#     author: str
+#     text: str
+#     is_private: bool
