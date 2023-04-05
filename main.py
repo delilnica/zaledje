bcf6b6ab7c992d050b16b5ed32034f8198bff764
bertronika <nejc@bertoncelj.eu.org>
2023-04-05 22:00:24 +0200

    Predelaj API, loči prikaz spletne strani

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 from fastapi import FastAPI, Response, Request, HTTPException, Form, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_versioning import version, VersionedFastAPI
 from pydantic import BaseModel
 
@@ -23,45 +23,37 @@ Base.metadata.create_all(engine)
 import schemas
 
 app = FastAPI()
-templates = Jinja2Templates(directory="static")
 
-@app.get("/")
-@version(1)
-def home_page(request: Request):
-    """Domača stran Delilnice"""
-    return templates.TemplateResponse("index.html", {"request": request})
+origins = [
+    "http://localhost:8000",
+    "http://localhost:8001",
+]
 
-class AddForm:
-    def __init__(self, title: str = Form(...), author: str = Form(...), text: str = Form(...), is_private: str = Form(False)):
-        self.title = title
-        self.author = author
-        self.text = text
-        self.is_private = is_private
+# @app.get("/")
+# @version(1)
+# def home_page(request: Request):
+#     """Domača stran Delilnice"""
+#     return "To pa ne gre..."
 
-# @app.post("/add", status_code=201, response_class=HTMLResponse)
 @app.post("/add", status_code=201)
 @version(1)
-# def add_fragment(frag: schemas.Fragment, request: Request, response: Response):
-# def add_fragment(add_form: AddForm = Depends()):
-def add_fragment(request: Request, response: Response, add_form: AddForm = Depends()):
+def add_fragment(frag: schemas.Fragment, request: Request, response: Response):
     """Dodaj nov fragment"""
-
     client_host = request.client.host
-    print(add_form.is_private)
+
     #session = Session(bind=engine, expire_on_commit=False)
-    with Session(engine) as session:
+    with Session(engine, expire_on_commit=False) as session:
         fragment = Fragment(
             ip_addr=str(client_host),
             expiry_date="-",
-            title=add_form.title,
-            author=add_form.author,
-            text=add_form.text,
-            is_private=(add_form.is_private == "on")
+            title=frag.title,
+            author=frag.author,
+            text=frag.text,
+            is_private=frag.is_private
         )
 
         session.add(fragment)
         session.commit()
-        print(f"USPEH - Vnašam fragment, oznaka {str(fragment.id)}")
 
     return {"success": True, "id": fragment.id}
 
@@ -71,28 +63,23 @@ def retrieve_fragment(id: int, request: Request, response: Response):
     """Pridobi fragment po podanem ID-ju"""
 
     frag_list = []
-    # frag = None
 
     statement = select(Fragment).where(Fragment.id==id)
     with Session(engine) as session:
         for frag in session.scalars(statement):
             frag_list.append(frag)
-            # frag = session.scalars(statement)
 
     if len(frag_list) < 1:
-    # if frag == None:
         response.status_code = 404
         # TODO tudi za is_private
 
         # raise HTTPException(status_code=404, detail=f"Fragment {id} ne obstaja.")
 
-        print("NAPAKA - fragment ne obstaja")
         retval = {"success": False, "reason": "Ta fragment ne obstaja."}
     else:
-        print(f"USPEH - {frag_list}")
         retval = {"success": True, "fragment": frag_list[0]}
 
-    return templates.TemplateResponse("retrieve_fragment.html", {"request": request, "retval": retval})
+    return retval
 
 @app.get("/author/{author}")
 @version(1)
@@ -139,13 +126,11 @@ def retrieve_all_fragments(only_meta: bool=False):
     print(f"USPEH - {frag_list}")
     return {"success": True, "fragments": frag_list}
 
-# @app.delete("/delete/{id}")
-#@version(1)
-# def delete_fragment(id: int):
-#     """Izbriši fragment"""
-#
-#     return success
-
 app = VersionedFastAPI(app, version_format="{major}", prefix_format="/v{major}")
-# app.mount("/", StaticFiles(directory="static", html=True), name="static")
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
--- schemas.py
+++ schemas.py
@@ -1,21 +1,10 @@
 from pydantic import BaseModel
-from typing import Annotated
-from fastapi import Form
 
 class Fragment(BaseModel):
     """
     Podatki, javno izpostavljeni prek API-ja.
     """
-    title: Annotated[str, Form()]
-    author: Annotated[str, Form()]
-    text: Annotated[str, Form()]
-    is_private: Annotated[bool, Form()]
-
-# class Fragment(BaseModel):
-#     """
-#     Podatki, javno izpostavljeni prek API-ja.
-#     """
-#     title: str
-#     author: str
-#     text: str
-#     is_private: bool
+    title: str
+    author: str
+    text: str
+    is_private: bool
